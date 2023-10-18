58e2710cd493a9a3eebf1b698e568645d37c0526
Soyunnlee <soxxy95@naver.com>
2023-10-18 10:26:45 +0900

    datepicker update

--- src/lib/components/datepicker/RangePicker.svelte
+++ src/lib/components/datepicker/RangePicker.svelte
@@ -1,15 +1,11 @@
-<!-- https://github.com/beyonk-adventures/svelte-datepicker#svelte-kit-support svelte kit config 오류 확인 필요  -->
 <script lang="ts">
 	//#region import
-	import DatePicker from '@roylabs/svelte-datepicker-ts/src/components/DatePicker.svelte';
-	import { CalendarStyle } from './rangeCalendarStyle';
 	import type { Dayjs } from 'dayjs';
 	import dayjs from 'dayjs';
 	import { onMount } from 'svelte';
 	//#endregion
 
 	//#region Variable _ Style
-	const calendarStyle = new CalendarStyle();
 	const style = {
 		btn: `bg-[#131316] border border-[#434346] rounded-md focus:border-transparent focus:ring-1 focus:ring-[#3D3C41] pl-4 py-1 text-white 2xl:text-xl 2xl:py-1.5 `
 	};
@@ -35,12 +31,6 @@
 	//#endregion
 
 	//#region function
-	function onChnage() {
-		from = selected[0];
-		to = selected[1];
-		onRangeSelect(from.toDate(), to.toDate());
-	}
-
 	//#endregion
 
 	//#region mount
@@ -51,23 +41,21 @@
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<DatePicker styling={calendarStyle} range={true} time={true} bind:selected on:change={onChnage}>
-	<button type="button" class={style.btn} style="padding-left: 0;">
-		<div class="flex items-center justify-center divide-x divide-gray-600">
-			<span class="px-2 text-gray-100 xl:px-3">
-				<!-- Icon -->
-				<img
-					src="https://i.esdrop.com/d/f/KRQCESGJO3/v7ZUt823Rd.png"
-					alt=""
-					class="h-4 w-4 2xl:h-5 2xl:w-5"
-				/>
-			</span>
-			<span class="px-2 xl:px-4 2xl:text-lg">
-				{'기간 선택'}
-			</span>
-		</div>
-	</button>
-</DatePicker>
+<button type="button" class={style.btn} style="padding-left: 0;">
+	<div class="flex items-center justify-center divide-x divide-gray-600">
+		<span class="px-2 text-gray-100 xl:px-3">
+			<!-- Icon -->
+			<img
+				src="https://i.esdrop.com/d/f/KRQCESGJO3/v7ZUt823Rd.png"
+				alt=""
+				class="h-4 w-4 2xl:h-5 2xl:w-5"
+			/>
+		</span>
+		<span class="px-2 xl:px-4 2xl:text-lg">
+			{'기간 선택'}
+		</span>
+	</div>
+</button>
 
 <style>
 </style>
